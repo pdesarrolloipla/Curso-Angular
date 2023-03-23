31dcedbf273433cd299e9c3ea76ce832fddd094d
adrian <p.desarrollo.ipla@gmail.com>
2023-03-23 13:59:35 +0100

    app-resultados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShareModule } from './share/share.module';
+import { GifsModule } from './gifs/gifs.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ShareModule,
+    GifsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
